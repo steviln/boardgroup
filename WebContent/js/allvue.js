ebb31c5092629bfd8415e38de03ceb7f4be4e239
Steinar Vilnes <steviln72@hotmail.com>
2021-05-02 20:00:07 +0200

    more Vue

--- WebContent/js/allvue.js
+++ WebContent/js/allvue.js
@@ -1,13 +1,26 @@
 
+	var startKnapp = Vue.component('sesson-change-button',
+	{ data: function(){
+		return {  }
+	},
+	created(){
+		console.log("button created");
+	},
+	props: { tekst: { type: String, default: 'Logg Inn ->'}, klasse: { type: String, default: 'vue-temp-hide' }},
+	template: '<button :class=klasse>{{ tekst }}</button>'});
 	
-	new Vue({
+	var logintop = new Vue({
 	
-		el: "#login-button-holder",
+		el: "#top-vue-login-element",
+		created(){
+			console.log("created changed");
+		},
+		components: { startKnapp },
 		methods: {
-			clicked: function(){
-				alert("klikket");
-			}
-		}
+		},
+
 	
 	});
+	
+	
 	
