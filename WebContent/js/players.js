115fb718f1ad4bf4c4269fd1e8e9d97e2f8617a2
Steinar Vilnes <sv@siteman.no>
2021-05-27 20:33:54 +0200

    change to ue

--- WebContent/js/players.js
+++ WebContent/js/players.js
@@ -3,12 +3,13 @@
 		el: "#player-edit-form",
 		data: function(){
 			return {
-				fornavn: "",
-				etternavn: "",
-				brukernavn: "",
-				passorden: "",
-				passordto: "",
-				epost: ""			}
+				fornavn: document.getElementById('fornavn').value,
+				etternavn: document.getElementById('etternavn').value,
+				brukernavn: document.getElementById('brukernavn').value,
+				passorden: document.getElementById('passorden').value,
+				passordto: document.getElementById('passordto').value,
+				epost: document.getElementById('epost').value			
+			}
 		},
 		methods: {
 
